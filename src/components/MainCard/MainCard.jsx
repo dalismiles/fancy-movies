5237b6f7202233aed5c7cd29be8366d469612b59
dalismiles <dali.iapichino@gmail.com>
2022-09-06 17:02:28 +0200

    feat:mixins

--- src/components/MainCard/MainCard.jsx
+++ src/components/MainCard/MainCard.jsx
@@ -8,24 +8,26 @@ const MainCard = ({ cardData, cardStyle = {} }) => {
   const [isModalVisible, setModalVisibility] = useState(false);
 
   return (
-    <div
-      className="MainCard"
-      style={cardStyle}
-      onClick={() => setModalVisibility(true)}
-    >
-      <img
-        className="MainCard__img"
-        src={`https://image.tmdb.org/t/p/w342${poster_path}`}
-        alt={title}
-      />
-      <div className="MainCard__text">
-        <h3 style={{ fontSize: cardStyle.fontSize }}>{original_title}</h3>
-        <p>{vote_average}</p>
+    <>
+      <div
+        className="MainCard"
+        style={cardStyle}
+        onClick={() => setModalVisibility(true)}
+      >
+        <img
+          className="MainCard__img"
+          src={`https://image.tmdb.org/t/p/w342${poster_path}`}
+          alt={title}
+        />
+        <div className="MainCard__text">
+          <h3 style={{ fontSize: cardStyle.fontSize }}>{original_title}</h3>
+          <p>{vote_average}</p>
+        </div>
       </div>
       {isModalVisible && (
         <Modal data={cardData} setModalVisibility={setModalVisibility} />
       )}
-    </div>
+    </>
   );
 };
 
